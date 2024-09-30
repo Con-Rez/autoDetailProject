12c08a35e456e3750cc7dce11bf81c7389151078
Con-Rez <newrealitywolfboy980@outlook.com>
2024-09-29 18:13:21 -0700

    Created a temporary about page, swapped the photos out for better ones, and removed navbar elements

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.homeView, name="home"),
+    path('about/', views.aboutView, name='about'),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -3,10 +3,16 @@ from django.http import HttpResponse # Allows for raw HTTP output commands withi
 import datetime; # Allows for the use of the datetime module to get the current date and time
 
 # Create your views here.
+
+# Create a new view for the home page
 def homeView(request):
     # This can override the HTML file in its entirety!
     # return HttpResponse("Hello world, this is a python file output!") 
 
     # Create and pass variable to home.html to say the current time
     today = datetime.datetime.now().date() 
-    return render(request,"home.html", {"today": today})
+    return render(request,"home.html", {"todayTag": today})
+
+# Create a new view for the about page, no arguments needed for it
+def aboutView(request):
+    return render(request,"about.html")
