9272f021b4e8a7efe179c7cbdd072889798954ba
katerinacowan <katerina@cowan.me>
2025-03-23 22:17:08 -0700

    fix settingfs again

--- autoDetailProject/settings.py
+++ autoDetailProject/settings.py
@@ -135,10 +135,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = 'static/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
